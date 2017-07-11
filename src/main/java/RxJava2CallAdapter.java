6155beed5c152bc0de4ad84a20ede44ae1d11c2f
siwarat.s <siwarat.s@alphafast.com>
2017-07-11 13:55:48 +0700

    first commit

---
+++ src/main/java/RxJava2CallAdapter.java
@@ -0,0 +1,74 @@
+import io.reactivex.BackpressureStrategy;
+import io.reactivex.Observable;
+import io.reactivex.Scheduler;
+import retrofit2.Call;
+import retrofit2.CallAdapter;
+import retrofit2.Response;
+
+import java.lang.reflect.Type;
+
+/**
+ * Created by siwarats on 11/7/2560.
+ */
+public class RxJava2CallAdapter<R> implements CallAdapter<R, Object> {
+    private final Type responseType;
+    private final Scheduler scheduler;
+    private final boolean isAsync;
+    private final boolean isResult;
+    private final boolean isBody;
+    private final boolean isFlowable;
+    private final boolean isSingle;
+    private final boolean isMaybe;
+    private final boolean isCompletable;
+
+    RxJava2CallAdapter(Type responseType, Scheduler scheduler, boolean isAsync, boolean isResult,
+                       boolean isBody, boolean isFlowable, boolean isSingle, boolean isMaybe,
+                       boolean isCompletable) {
+        this.responseType = responseType;
+        this.scheduler = scheduler;
+        this.isAsync = isAsync;
+        this.isResult = isResult;
+        this.isBody = isBody;
+        this.isFlowable = isFlowable;
+        this.isSingle = isSingle;
+        this.isMaybe = isMaybe;
+        this.isCompletable = isCompletable;
+    }
+
+    @Override public Type responseType() {
+        return responseType;
+    }
+
+    @Override public Object adapt(Call<R> call) {
+        Observable<Response<R>> responseObservable = isAsync
+                ? new CallEnqueueObservable<>(call)
+                : new CallExecuteObservable<>(call);
+
+        Observable<?> observable;
+        if (isResult) {
+            observable = new ResultObservable<>(responseObservable);
+        } else if (isBody) {
+            observable = new BodyObservable<>(responseObservable);
+        } else {
+            observable = responseObservable;
+        }
+
+        if (scheduler != null) {
+            observable = observable.subscribeOn(scheduler);
+        }
+
+        if (isFlowable) {
+            return observable.toFlowable(BackpressureStrategy.LATEST);
+        }
+        if (isSingle) {
+            return observable.singleOrError();
+        }
+        if (isMaybe) {
+            return observable.singleElement();
+        }
+        if (isCompletable) {
+            return observable.ignoreElements();
+        }
+        return observable;
+    }
+}
